756320a091ff102fec02a3b3790a1637bf8f2a29
Hiddify Agent Traffic Manager <agent-traffic-manager@hiddify.com>
2025-12-04 21:30:13 +0330

    Fix setup.py to manually specify packages

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Define package name (with underscores for Python import)
-PACKAGE_NAME = "hiddify_agent_traffic_manager"
-
 setup(
     name="hiddify-agent-traffic-manager",
     version="1.0.0",
@@ -17,12 +14,14 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/smmnouri/hiddify-agent-traffic-manager",
-    # Use find_packages but rename the root package
-    packages=[pkg.replace('hiddify-agent-traffic-manager', PACKAGE_NAME) if 'hiddify-agent-traffic-manager' in pkg else pkg 
-              for pkg in find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*'])],
-    # Or manually specify packages
-    # packages=[PACKAGE_NAME, f"{PACKAGE_NAME}.models", f"{PACKAGE_NAME}.utils", f"{PACKAGE_NAME}.tasks", f"{PACKAGE_NAME}.admin", f"{PACKAGE_NAME}.api"],
-    package_dir={PACKAGE_NAME: '.'},
+    # Manually specify packages since we're in the root directory
+    packages=['hiddify_agent_traffic_manager',
+              'hiddify_agent_traffic_manager.models',
+              'hiddify_agent_traffic_manager.utils',
+              'hiddify_agent_traffic_manager.tasks',
+              'hiddify_agent_traffic_manager.admin',
+              'hiddify_agent_traffic_manager.api'],
+    package_dir={'hiddify_agent_traffic_manager': '.'},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
